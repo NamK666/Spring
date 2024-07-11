06896e2fbd932d9e3b43d0120590d31ad3ee950b
NamK666 <chriscole77@naver.com>
2024-07-11 17:23:31 +0900

    update

--- src/main/java/day0709/ThymeleafController.java
+++ src/main/java/day0709/ThymeleafController.java
@@ -3,18 +3,15 @@ package day0709;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThymeleafController {
 
     @GetMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("name", "굿굿베리굿");
-        model.addAttribute("testing", "테스팅 테스팅");
-        model.addAttribute("h1", "h1은 이런식으로 나옵니다");
-        model.addAttribute("h2", "h2도 연습해보자");
-        model.addAttribute("h3style", "color:red");
-        model.addAttribute("h3", "h3은 스타일과 함께 해보자");
-        return "index"; // 여기서 index라는 이름으로 찾는 것임.
+    public String index(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name1", name);
+        return "index";
     }
 }
+
