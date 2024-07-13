ee58f648a6c29f47d89e51b1d62d1c319ba5bf86
NamK666 <chriscole77@naver.com>
2024-07-13 23:24:49 +0900

    update

--- src/main/java/com/example/basic/BasicApplication.java
+++ src/main/java/com/example/basic/BasicApplication.java
@@ -9,21 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "day0709")
-
+@SpringBootApplication
 public class BasicApplication {
-
-    @Value("${server.port}") // yml에서 server, port를 가져오기.
-    private int port;
-
-    @Value("${spring.application.name}")
-    private String appName;
-
-    @PostConstruct
-    public void printConfig() {
-        System.out.println("포트번호: " + port);
-        System.out.println("애플리케이션 이름: " + appName);
-    }
 
     public static void main(String[] args) {
 
